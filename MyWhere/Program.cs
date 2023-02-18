56616b4d05b8b1876c9e8f67a5a93a08bdecad11
Sarugen <sepenzev97@gmail.com>
2023-02-18 17:40:43 +0200

    Add project files.

---
+++ MyWhere/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+var list = new List<int> { 1, 2, 3, 4 };
+
+var oddQuery = list.Select(Filter); // MyWhere - ваш метод
+bool Filter (int i)
+{ 
+    return i % 2 != 0;
+}
+
+list.Add(5);
+
+var filterredList = oddQuery.ToList(); // 1, 3, 5
+
+
+/*var list = new List<int> { 1, 2, 3, 4 };
+
+var oddQuery = list.Where(i => i % 2 != 0);
+
+    
+    
+
+list.Add(5);
+var filterredList = oddQuery.ToList(); // 1, 3, 5
+*/
